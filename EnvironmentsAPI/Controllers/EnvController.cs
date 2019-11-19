c5f102caf33823d659cb24d496edeb88f44cc515
Dan Patrascu-Baba <dan@danpatrascu.com>
2019-11-19 20:26:11 +0200

    Add Env API

---
+++ EnvironmentsAPI/Controllers/EnvController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace EnvironmentsAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnvController : ControllerBase
+    {
+
+        private readonly IOptions<Env> _env;
+        public EnvController(IOptions<Env> env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        public Env Get()
+        {
+            return _env.Value;
+        }
+    }
+}
---
+++ EnvironmentsAPI/Env.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnvironmentsAPI
+{
+    public class Env
+    {
+        public string Name { get; set; }
+        public string SettingsFileName { get; set; }
+    }
+}
